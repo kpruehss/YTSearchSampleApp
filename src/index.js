4d0281ce88367a45084020bbb5c07d10c0072976
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-26 20:06:16 +1000

    ADD: All video components + logic

--- src/index.js
+++ src/index.js
@@ -14,19 +14,31 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null 
+    };
+    
+    this.videoSearch(`surfboards`);
+  }
 
-    YTSearch({key: API_KEY, term: `surfboards`}, (videos) => {
-      this.setState({ videos }); // only works if argument name === key name
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      }); // only works if argument name === key name
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video = {this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos = {this.state.videos} />
       </div>
     );
   }
