ea9ef9da0d4d56c576c67ad94bca1de107312e97
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-26 15:56:12 +1000

    REFACTOR: functional App component => Class component; ADD: YT state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 /*eslint-disable no-unused-vars*/ 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyACMf0dj07TJq7GSwQVrbxVsRHQeH5QLEU';
 
 
 // Create a new component. This component should produce some HTML
-const App = () => (
-  <div>
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos }); // only works if argument name === key name
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
 
 // Take this component's generated HTML and put it in the DOM
 ReactDom.render(<App />, document.querySelector('.container'));
