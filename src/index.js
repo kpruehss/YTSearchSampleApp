896aa48afe8fbe494cba4b1dca48c960af945b53
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-26 13:40:39 +1000

    ADD: Youtube Data API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom';
+
+const API_KEY = 'AIzaSyACMf0dj07TJq7GSwQVrbxVsRHQeH5QLEU';
+
+
 // Create a new component. This component should produce some HTML
 const App = () => {
   return <div>Hi!</div>;
